1b7b38ce22bfa16e922819e7d6b51f2a8dde4eca
Ceminay35 <cemce2435@gmail.com>
2026-06-29 16:30:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Orbi görselini doğrudan depodan çağırıyoruz (Linkle uğraşma)
-st.image("orbi.png", width=200) 
+st.image("orbi_ai.png", width=200) 
 
 st.title("Orbi: Dünya Kaşifi 🌍")
 # ... geri kalan kodlar aynı ...
